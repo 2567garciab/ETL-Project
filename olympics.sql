fb822320e5b461d4f9927f439814ab0bc21506ee
siqing cao <caosiqingcsq@yahoo.com>
2020-05-21 21:09:01 -0500

    New Files

--- olympics.sql
+++ olympics.sql
@@ -3,40 +3,43 @@ drop table if exists olympic_12 cascade;
 drop table if exists olympic_16 cascade;
 
 create table "olympic_08"(
-      "name" varchar(30),
-      "sex" varchar(10),
-      "team" varchar(30),
-      "year" int,
-      "sport" varchar(30),
-      "event" varchar(30),
-      "medal" varchar(10),
+      "First_Name" varchar(30),
+	  "Last_Name" varchar(30),
+      "Gender" varchar(10),
+      "Country" varchar(30),
+      "Year" int,
+      "Sport" varchar(30),
+      "Event" varchar(100),
+      "Medal" varchar(10),
       constraint "pk_olympic_08" primary key(
-	  "name", "event"
+	  "First_Name","Last_Name","Event","Medal"
 	  )
 );
 
 create table "olympic_12"(
-      "name" varchar(30),
-      "sex" varchar(10),
-      "team" varchar(30),
-      "year" int,
-      "sport" varchar(30),
-      "event" varchar(30),
-      "medal" varchar(10),
+      "First_Name" varchar(30),
+	  "Last_Name" varchar(30),
+      "Gender" varchar(10),
+      "Country" varchar(30),
+      "Year" int,
+      "Sport" varchar(30),
+      "Event" varchar(100),
+      "Medal" varchar(10),
       constraint "pk_olympic_12" primary key(
-	  "name", "event"
+	  "First_Name","Last_Name","Event"
 	  )
 );
 
 create table "olympic_16"(
-      "name" varchar(30),
-      "sex" varchar(10),
-      "team" varchar(30),
-      "year" int,
-      "sport" varchar(30),
-      "event" varchar(30),
-      "medal" varchar(10),
+      "First_Name" varchar(30),
+	  "Last_Name" varchar(30),
+      "Gender" varchar(10),
+      "Country" varchar(30),
+      "Year" int,
+      "Sport" varchar(30),
+      "Event" varchar(100),
+      "Medal" varchar(10),
       constraint "pk_olympic_16" primary key(
-	  "name", "event"
+	  "First_Name","Last_Name","Event"
 	  )
 );
